18c1a4a7ce6182784b178e28d2be20ce98717ba7
JareBear <jaredgrozz@gmail.com>
2020-04-16 14:56:00 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,25 +5,82 @@ from PyQt5 import *
 from PyQt5 import uic, QtPrintSupport
 from PyQt5.QtPrintSupport import QPrintDialog, QPrinter
 from PyQt5 import QtCore, QtWidgets, QtPrintSupport, QtGui
-import sys, os, json, spotdl
-
+import sys, os, json, spotdl, re, threading
+import time
 Data_JSON_Contents = {}
 Data_JSON = 'settings.json'
 download_to_directory = []
 theme = []
 always_on_top = []
 originalPalette = None
-class ConvertThread(QThread):  
-    data_downloaded = pyqtSignal(object)
+
+download_percentage = 0
+
+file_calculate_percentage = ''
+
+class CalculateThread(QThread):  
+    calculate = pyqtSignal(int)
     def __init__(self, file):
         QThread.__init__(self)
         self.file = file
     def run(self):
-        # j = self.file
-        self.data_downloaded.emit(f'1/{len(self.file)} - Starting.')
+        global download_percentage
+        isRunning = True
+        self.maxLen = None
+        print(file_calculate_percentage)
+        while True:
+            if not file_calculate_percentage == '':
+                if self.maxLen == None: self.maxLen = sum(1 for line in open(file_calculate_percentage))
+                num_lines = sum(1 for line in open(file_calculate_percentage))
+                download_percentage = (self.maxLen-num_lines)/self.maxLen*100
+                self.calculate.emit(download_percentage)
+                if num_lines == 0:
+                    self.maxLen = None
+                    isRunning = False
+                    break
+            time.sleep(0.1)
+class DownloadThread(QThread):  
+    data_downloaded = pyqtSignal(object)
+    def __init__(self, url):
+        QThread.__init__(self)
+        self.url = url
+    def run(self):
+        global file_calculate_percentage
+        self.data_downloaded.emit(f'Starting..')
         # time.sleep(1)
-        print('Starting download')
-        print(self.file)
+        url = self.url
+        if '/track/' in url:
+            self.data_downloaded.emit(f' Downloading track...')
+            os.popen(f'spotdl --folder "{download_to_directory[0]}" --song {url} --overwrite skip').read() # > test.txt'
+        elif '/playlist/' in url:
+            self.data_downloaded.emit(f'Getting all songs from playlist...')
+            os.popen(f'spotdl --folder "{download_to_directory[0]}" --playlist {url} --overwrite skip').read()
+            self.data_downloaded.emit(f'Downloading all songs from playlist....')
+            fileName = ''
+            fileList = []
+            for file in os.listdir(os.path.dirname(os.path.abspath(__file__))):
+                if file.endswith(".txt"): fileList.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), file))
+            fileName = fileList[0]
+            file_calculate_percentage = fileName
+            # threading.Thread(target=self.calculate_progress,args=(fileName,)).start()
+            os.popen(f'spotdl --folder "{download_to_directory[0]}"  --list "{fileName}" --overwrite skip').read()
+        elif '/album/' in url:
+            self.data_downloaded.emit(f'Getting all songs from album...')
+            os.popen(f'spotdl --folder "{download_to_directory[0]}" --album {url} --overwrite skip').read()
+            self.data_downloaded.emit(f'Downloading all songs from album....')
+            fileName = ''
+            fileList = []
+            for file in os.listdir(os.path.dirname(os.path.abspath(__file__))):
+                if file.endswith(".txt"): fileList.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), file))
+            fileName = fileList[0]
+            file_calculate_percentage = fileName
+            # threading.Thread(target=self.calculate_progress,args=(fileName,)).start()
+            os.popen(f'spotdl --folder "{download_to_directory[0]}"  --list "{fileName}" --overwrite skip').read()
+        self.data_downloaded.emit('Finished Downloading!')
+        time.sleep(1)
+        self.data_downloaded.emit('')
+        file_calculate_percentage = ''
+        os.remove(fileName)
 class mainwindowUI(QMainWindow):
     resized = QtCore.pyqtSignal()
     def __init__(self, parent = None):
@@ -32,8 +89,17 @@ class mainwindowUI(QMainWindow):
         if originalPalette == None: originalPalette = QApplication.palette()
         super(mainwindowUI, self).__init__(parent)
         uic.loadUi('UI/mainwindo.ui', self)
+        self.setStyleSheet(open("style.qss", "r").read())
+        
         self.setWindowTitle('Spotify Downloader')
         self.set_theme()
+        
+        self.txtURL = self.findChild(QLineEdit, 'url')
+        self.txtURL.setToolTip('Provide a spotify link.')
+        
+        self.btnDownload = self.findChild(QPushButton,'btnDownload')
+        self.btnDownload.clicked.connect(self.start_download)
+        self.btnDownload.setToolTip('Downloads the link provided in the textbox.')
         
         self.progressBar = self.findChild(QProgressBar, 'progressBar')
         self.progressBar.setHidden(True)
@@ -43,9 +109,52 @@ class mainwindowUI(QMainWindow):
         self.actionAbout.triggered.connect(self.open_about)
         self.actionAbout.setIcon(self.style().standardIcon(getattr(QStyle, 'SP_FileDialogInfoView')))
 
+        self.actionDownload_location = self.findChild(QAction, 'actionDownload_location')
+        self.actionDownload_location.triggered.connect(self.get_download_dir)
+        self.actionDownload_location.setIcon(self.style().standardIcon(getattr(QStyle, 'SP_DirOpenIcon')))
+        self.actionDownload_location.setStatusTip('Set default download location.')
+
+        self.actionAlways_appear_on_top = self.findChild(QAction, 'actionAlways_appear_on_top')
+        self.actionAlways_appear_on_top.triggered.connect(self.set_appear_on_top)
+        self.actionAlways_appear_on_top.setChecked(True if always_on_top[0] == 'True' else False)
+        self.actionAlways_appear_on_top.setStatusTip('The program will appear always on top. (The program will restart)')
+
+        if always_on_top[0] == 'True': self.setWindowFlags(Qt.WindowStaysOnTopHint)
+
         self.actionTheme = self.findChild(QAction, 'actionTheme')
         self.actionTheme.triggered.connect(self.open_theme)
         self.show()
+    def get_download_dir(self):
+        dir_ = QFileDialog.getExistingDirectory(None, 'Select a default download folder', 'C:\\', QFileDialog.ShowDirsOnly)
+        if dir_:
+            temp_num = theme[0]
+            temp_appear_on_top = always_on_top[0]
+            print(dir_)
+            Data_JSON_Contents.pop(0)
+            Data_JSON_Contents.append(
+                {
+                    "Download Path": [dir_],
+                    "Appear on top": [temp_appear_on_top],
+                    "Theme": [temp_num]
+                })
+            with open(Data_JSON, mode='w+', encoding='utf-8') as file: json.dump(Data_JSON_Contents, file, ensure_ascii=True, indent=4)
+            load_data_file(always_on_top, download_to_directory, theme)
+    def set_appear_on_top(self):
+        temp_num = theme[0]
+        temp_path = download_to_directory[0]
+        Data_JSON_Contents.pop(0)
+        Data_JSON_Contents.append(
+            {
+                "Download Path": [temp_path],
+                "Appear on top": [str(self.actionAlways_appear_on_top.isChecked())],
+                "Theme": [temp_num]
+            })
+        with open(Data_JSON, mode='w+', encoding='utf-8') as file: json.dump(Data_JSON_Contents, file, ensure_ascii=True, indent=4)
+        load_data_file(always_on_top, download_to_directory, theme)
+        
+        self.mainwindow = mainwindowUI()
+        self.mainwindow.show()
+        self.close()
     def set_theme(self):
         if theme[0] == 0: 
             app.setStyle("windowsvista")
@@ -78,32 +187,39 @@ class mainwindowUI(QMainWindow):
         self.settings = settingsUI()
         self.settings.resize(300,100)
         self.settings.show()
-    def start_conversion(self, files):
-        self.threads = []
-        converter = ConvertThread(files)
-        converter.data_downloaded.connect(self.on_data_ready)
-        self.threads.append(converter)
-        converter.start()
+    def start_download(self):
+        if self.txtURL.text() == '': return
+        p = re.compile('https://open.spotify.com/(track|playlist|album)/')
+        m = p.match(self.txtURL.text())
+        if m:
+            self.threads = []
+            downloader = DownloadThread(self.txtURL.text())
+            downloader.data_downloaded.connect(self.on_data_ready)
+            self.threads.append(downloader)
+            downloader.start()
+            
+            # self.otherthreads = []
+            calculator = CalculateThread('')
+            calculator.calculate.connect(self.update_progress_bar)
+            self.threads.append(calculator)
+            calculator.start()
+            
+        else:
+            QMessageBox.critical(self, 'Doesn\'t Match', f"The spotfiy link: \n'{self.txtURL.text()}' isn't valid.", QMessageBox.Ok, QMessageBox.Ok)
     def on_data_ready(self, text):
         try:
             # self.lblState.setHidden(False)
-            self.progressBar.setHidden(False)
-            if not text == 'Finished!':
-                if not text == '':
-                    currentNum = text.split('/')
-                    currentNum = int(currentNum[0])
-                    maxnum = text.split('/')
-                    maxnum = maxnum[1]
-                    maxnum = maxnum.split(' - ')
-                    maxnum = int(maxnum[0])
-                    self.progressBar.setValue(currentNum)
-                    self.progressBar.setMaximum(maxnum)
-            self.progressBar.setFormat(' ' + text)
             if text == '': 
                 self.progressBar.setHidden(True)
-        except Exception as e:
-            print(e)
-
+                return
+            self.progressBar.setMaximum(100)
+            self.progressBar.setHidden(False)
+            self.progressBar.setFormat(' ' + text)
+        except Exception as e: print(e)
+    def update_progress_bar(self, value):
+        self.progressBar.setValue(download_percentage)
+        self.progressBar.setMaximum(100)
+        print(value)
 class settingsUI(QWidget):
     def __init__(self):
         super().__init__()
@@ -194,17 +310,10 @@ if __name__ == '__main__':
     if not os.path.isfile(Data_JSON):
         path = os.path.dirname(os.path.abspath(__file__)) + '/Downloads'
         path = path.replace('\\', '/')
-        with open(Data_JSON, 'w+') as f: f.write('[{"Appear on top":["False"],"Download Path":["' + path + '}"],"Theme":[0]}]')
+        with open(Data_JSON, 'w+') as f: f.write('[{"Appear on top":["False"],"Download Path":["' + path + '"],"Theme":[0]}]')
     # Load data file
     load_data_file(always_on_top, download_to_directory, theme)
     # start GUI
     app = QApplication(sys.argv)
     window = mainwindowUI()
     sys.exit(app.exec_())
-# url = 'https://open.spotify.com/track/2DGa7iaidT5s0qnINlwMjJ'
-
-# if 'track' in url:
-#     os.popen(f'spotdl --song {url}').read()
-# elif 'playlist' in url:
-#     os.popen(f'spotdl --playlist {url}').read()
-#     # os.popen(f'spordl --list {filename}').read()
